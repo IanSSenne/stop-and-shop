2e9b0f1f8043df90d174d1d876162c59e445836a
Michael Reyna <25.27.reyna@gmail.com>
2023-03-10 12:33:29 -0700

    added to typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,51 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Item {
+        _id: ID 
+        title: String
+        photos: [String]
+        Location: UNKNOWN
+        datePosted: DateTime
+        ask: Number
+        tags: [Tag]
+    }
+
+    type User {
+        _id: ID 
+        bookmarkedItems: [Item]
+        sellingItems: [Item]
+        purchasedItems: [Item]
+        displayName: String
+        email: String
+        Password: String
+        Chats: [Chat]
+        Interests: [Tag]
+    }
+
+    type Chat {
+        _id: ID 
+        visibleTo: [User]
+        Message: [{
+        from: User,
+        message: String
+        timestamp: DateTime
+        offer: number (nullable)
+        }]
+    }
+
+    type Tag {
+        _id: ID 
+        name: String
+        color: String
+    }
+    type Query {
+    
+    }
+
+    type Mutation {
+    
+    }
+`;
+
+module.exports = typeDefs; 
