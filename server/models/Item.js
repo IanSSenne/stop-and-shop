98489423cf289e48406a0845b88dbb20a87f7667
Ian Senne <ian@iansenne.com>
2023-03-10 13:45:44 -0800

    format files

--- server/models/Item.js
+++ server/models/Item.js
@@ -1,34 +1,33 @@
 const { Schema, model } = require("mongoose");
 
-
-const itemSchema = new Schema ({
-    title: {
-        type: String,
-        required: true,
-        minlength: 1,
-        maxlength: 100,
-    },
-    photos: {
-        type: [String],
-    },
-    location: {
-        type: String,
-    },
-    datePosted: {
-        type: Date,
-    },
-    ask: {
-        type: Number,
-        required: true,
-    },
-    tags: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Tag",
-        },
-    ],
+const itemSchema = new Schema({
+	title: {
+		type: String,
+		required: true,
+		minlength: 1,
+		maxlength: 100,
+	},
+	photos: {
+		type: [String],
+	},
+	location: {
+		type: String,
+	},
+	datePosted: {
+		type: Date,
+	},
+	ask: {
+		type: Number,
+		required: true,
+	},
+	tags: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: "Tag",
+		},
+	],
 });
 
-const Item = model('Item', itemSchema);
+const Item = model("Item", itemSchema);
 
 module.exports = Item;
