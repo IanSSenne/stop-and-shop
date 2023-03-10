dd380c0eb6e3aa871b922e58e7f97efffab5957a
SpectreKilo <johnho8788@gmail.com>
2023-03-10 14:18:36 -0500

    finished User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema(
     {
@@ -27,10 +28,41 @@ const userSchema = new Schema(
             {
             type: Schema.Types.ObjectId,
             ref: "Item"
+            },
+        ],
+        purchasedItems: [
+            {
+            type: Schema.Types.ObjectId,
+            ref: "Item"
+            },
+        ],
+        chats: [
+            {
+            type: Schema.Types.ObjectId,
+            ref: "Chat"
             }
-        ]
+        ],
+        interests: [
+            {
+            type: Schema.Types.ObjectId,
+            ref: "Tag"
+            }
+        ],
     }
 );
+
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+  
+  userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+  };
 
 const User = model('User', userSchema);
 
