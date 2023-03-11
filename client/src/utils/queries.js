78c3e9ce13aff1db4f8e74ab1104e3a74db4f287
Michael Reyna <25.27.reyna@gmail.com>
2023-03-10 18:15:43 -0700

    added queries and mutations

---
+++ client/src/utils/queries.js
@@ -0,0 +1,18 @@
+import { gql } from '@apollo/client';
+
+
+export const QUERY_ITEMS = gql`
+    query getItems()
+`;
+
+export const QUERY_TAGS = gql`
+
+`;
+
+export const QUERY_SINGLE_ITEM = gql`
+
+`;
+
+export const QUERY_USER = gql`
+
+`;
