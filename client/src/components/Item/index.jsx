e32e90048ace545bc111ecca1f040bf6cc57dc12
SpectreKilo <johnho8788@gmail.com>
2023-03-11 18:50:49 -0500

    formatting

--- client/src/components/Item/index.jsx
+++ client/src/components/Item/index.jsx
@@ -5,19 +5,17 @@ import { Button, Card, Elevation } from "@blueprintjs/core";
 function Item(item) {
 	return (
 		<Link to={`/view/${item._id}`}>
-		<Card interactive={true} elevation={Elevation.TWO}>
-			<h3>
-            {item.title}
-			</h3>
-			<p>
-            Price: ${item.ask} <br/>
-            Photos: {item.photos[0]} <br/>
-            Date Posted: {item.datePosted} <br/>
-            Location: {item.location} <br/>
-            </p>
-			<Button>Details</Button>
-		</Card>
-        </Link>
+			<Card interactive={true} elevation={Elevation.TWO}>
+				<h3>{item.title}</h3>
+				<p>
+					Price: ${item.ask} <br />
+					Photos: {item.photos[0]} <br />
+					Date Posted: {item.datePosted} <br />
+					Location: {item.location} <br />
+				</p>
+				<Button>Details</Button>
+			</Card>
+		</Link>
 	);
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,10 +8,10 @@ import BpNavbar from "../components/NavBar";
 export const Home = () => {
 	const { loading, data } = useQuery(QUERY_ALL_ITEMS);
 	return (
-	<>
-	<BpNavbar/>
-	
-	{loading ? <h1>loading...</h1> : data.items.map((item) => <Item {...item} key={item._id} />)}
-	</>
-	)
+		<>
+			<BpNavbar />
+
+			{loading ? <h1>loading...</h1> : data.items.map((item) => <Item {...item} key={item._id} />)}
+		</>
+	);
 };
