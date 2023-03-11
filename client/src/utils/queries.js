99be2b83244757bee092af6df0621306d5c39bbd
SpectreKilo <johnho8788@gmail.com>
2023-03-10 21:12:38 -0500

    format and seed data

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,72 +1,70 @@
-import { gql } from '@apollo/client';
-
+import { gql } from "@apollo/client";
 
 export const QUERY_ITEMS = gql`
-    query getItems($tag: ID){
-        items(tag: $tag){ 
-            _id
-            title
-            photos
-            Location
-            datePosted
-            ask
-            tag {
-                _id
-            }
-        }
-    }
+	query getItems($tag: ID) {
+		items(tag: $tag) {
+			_id
+			title
+			photos
+			Location
+			datePosted
+			ask
+			tag {
+				_id
+			}
+		}
+	}
 `;
 
 export const QUERY_TAGS = gql`
-    {
-        tags {
-            _id
-            name
-            color
-        }
-    }
+	{
+		tags {
+			_id
+			name
+			color
+		}
+	}
 `;
 
 export const QUERY_SINGLE_ITEM = gql`
-    {
-        items {
-            _id
-            title
-            photos
-            Location
-            datePosted
-            ask
-            tag {
-                _id
-                name
-                color
-            }
-        }
-    }
+	{
+		items {
+			_id
+			title
+			photos
+			Location
+			datePosted
+			ask
+			tag {
+				_id
+				name
+				color
+			}
+		}
+	}
 `;
 
 export const QUERY_USER = gql`
-    {
-        user {
-            _id
-            bookmarkedItems
-            sellingItems
-            purchasedItems
-            displayName
-            email
-            password
-            Chats {
-                chat {
-                    _id
-                }
-            }
-            Interests {
-                tag {
-                    _id
-                    name
-                }
-            }
-        }
-    }
-
+	{
+		user {
+			_id
+			bookmarkedItems
+			sellingItems
+			purchasedItems
+			displayName
+			email
+			password
+			Chats {
+				chat {
+					_id
+				}
+			}
+			Interests {
+				tag {
+					_id
+					name
+				}
+			}
+		}
+	}
 `;
--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -3,4 +3,3 @@ const { User, Item, Tag } = require("../models");
 const userSeeds = require("./userSeeds.json");
 const itemSeeds = require("./itemSeeds.json");
 const tagSeeds = require("./tagSeeds.json");
-
