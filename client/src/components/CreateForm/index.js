d75d7a688d953f9f3c27f47dd4d4624a8111a18a
Michael Reyna <25.27.reyna@gmail.com>
2023-03-12 16:59:24 -0600

    created createform

---
+++ client/src/components/CreateForm/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { InputGroup } from '@blueprintjs/core'
+import { ADD_ITEM } from '../../utils/mutations';
+ 
+function CreateForm () {
+
+    return (
+
+    );
+}
+
+export default CreateForm;
