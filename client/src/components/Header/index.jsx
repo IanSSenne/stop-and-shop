82665ef80926230ecdaca3eedd9f490c4a7009bf
SpectreKilo <johnho8788@gmail.com>
2023-03-12 14:06:32 -0400

    created Header component and added to Home

---
+++ client/src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Card, Elevation } from "@blueprintjs/core";
+
+function Header() {
+return (
+    <header>
+        <div>
+            <Link to="/">
+                <h1>Stop-n-Shop</h1>
+            </Link>
+        </div>
+    </header>
+)
+};
+
+export default Header;
