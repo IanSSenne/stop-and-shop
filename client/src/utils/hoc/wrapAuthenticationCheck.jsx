49e537f7cd0fc537617696dfdcd807f51789a41d
SpectreKilo <johnho8788@gmail.com>
2023-03-13 18:24:53 -0400

    chat works

--- client/src/utils/hoc/wrapAuthenticationCheck.jsx
+++ client/src/utils/hoc/wrapAuthenticationCheck.jsx
@@ -2,7 +2,7 @@ import { useAuth } from "../../contexts/Auth";
 export function wrapAuthenticationCheck(Component) {
 	return function WrappedAuthenticationCheckComponent({ is, fallback = null, ...passonProps }) {
 		const auth = useAuth();
-		if (auth.user?.data) {
+		if (auth.user) {
 			return <Component {...passonProps} />;
 		}
 		return fallback;
