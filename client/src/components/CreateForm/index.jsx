8b988316e996b0deac4473af54c3928f93defb9e
Michael Reyna <25.27.reyna@gmail.com>
2023-03-14 00:39:52 -0600

    added form data

--- client/src/components/CreateForm/index.jsx
+++ client/src/components/CreateForm/index.jsx
@@ -1,43 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { FileInput, FormGroup, InputGroup, TextArea, ButtonGroup, Button } from '@blueprintjs/core'
 import { ADD_ITEM } from '../../utils/mutations';
  
 function CreateForm () {
+    const [item, setItem ] = useState();
+
+    function saveItem (event) {
+        event.preventDefault();
+
+        const [createNewItem, { loading: creatingItem }] = useMutation(ADD_ITEM);
+
+        const formData = new FormData(event.target.value);
+        const title = formData.get("title");
+        const description = formData.get("description");
+        const price = formData.get("price");
+        const tags = formData.get("tag");
+        const location = formData.get("location");
+        // TODO: figure out cloudinary
+        const itemData = {
+            title,
+            description,
+            price,
+            tags,
+            location
+        };
+
+        createNewItem({
+            variables: itemData,
+        }).then(() => {
+            event.target.useNavigate("/profile");
+        });
+    }
 
     return (
         <>
             <h1> Create Form</h1>
-            <section>
-                <article>
-                    <h1>Item Title</h1>
+            <FormGroup>
+                <form onSubmit={() => {
+                    createNewItem();
+                }}>
+                    <h3>Item Title</h3>
                     <InputGroup
+                        name="title"
                         placeholder='Enter the title of your new item...'
                         type='text'
                     />
-                    <h1>Item Price</h1>
-                    <InputGroup 
-                        placeholder='Enter the price of your new item...'
-                    />
-                    <h1>Item Description</h1>
-                    <TextArea 
-                        placeholder='Description...'
-                    />
-                    <h1>Tags</h1>
-                    <InputGroup 
-                        placeholder='Add tags...'
-                    />
-                    <h1>Location</h1>
+                    <h3>Item Price</h3>
                     <InputGroup
-                        placeholder='Enter your location...'
+                        name="price" 
+                        placeholder='Enter the price of your new item...'
+                        type='text'
                     />
-                </article>
-                <article>
-                    <h1>Add Item Images</h1>
-                    <FileInput text="Choose file..." icon="Document" buttonText="Upload..."  />
-                    <h1>Sell Your New Item</h1>
-                    <Button text='CONFIRM' rightIcon="tick"/>
-                </article>
-            </section>
+                    <h3>Item Description</h3>
+                    <TextArea 
+                        name="description"
+                        placeholder='Description...'
+                        type='text'
+                    />
+                    <h3>Tags</h3>
+                    <InputGroup 
+                        name="tags"
+                        placeholder='Add tags...'
+                        type='text'
+                    />
+                    <h3>Location</h3>
+                    <InputGroup
+                        name="location"
+                        placeholder='Enter your location...'
+                        type='text'
+                    />
+                    <h3>Add Item Images</h3>
+                    <FileInput name="images" text="Choose file..." icon="Document" buttonText="Upload..."  />
+                    <h3>Sell Your New Item</h3>
+                    <Button 
+                        type='submit'
+                        text='CONFIRM' 
+                        rightIcon="tick"
+                    />
+                </form>
+            </FormGroup>
         </>
     );
 }
