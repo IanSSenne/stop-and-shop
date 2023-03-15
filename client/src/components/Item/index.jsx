6ac445f7d8265e945d2bc9466560965fb262b4e5
Michael Reyna <25.27.reyna@gmail.com>
2023-03-15 11:43:05 -0600

    added a readme

--- client/src/components/Item/index.jsx
+++ client/src/components/Item/index.jsx
@@ -6,8 +6,8 @@ import { Button, Card, Elevation } from "@blueprintjs/core";
 function Item(item) {
 	return (
 		<Link to={`/view/${item._id}`}>
-			<Card className={styles.rule} interactive={true} elevation={Elevation.TWO}>
-				<h3>{item.title}</h3>
+			<Card className={styles.Card} interactive={true} elevation={Elevation.TWO}>
+				<h3 className={styles.title}>{item.title}</h3>
 				<p>
 					Price: ${item.ask} <br />
 					Photos: {item.photos[0]} <br />
