4e4b160f1180c9574821880c2f3a0033fb45ff5c
Ian Senne <ian@iansenne.com>
2023-03-15 09:32:54 -0700

    add front end ui for search

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,10 +1,29 @@
+import { Button, FormGroup, InputGroup, TagInput } from "@blueprintjs/core";
 import React from "react";
 import Header from "../components/Header";
-
 export const Search = () => {
+	const [tags, setTags] = React.useState([]);
+	const [query, setQuery] = React.useState("");
 	return (
 		<>
 			<Header />
+			<form>
+				<FormGroup label="Search">
+					<InputGroup
+						value={query}
+						onChange={(e) => {
+							setQuery(e.target.value);
+						}}></InputGroup>
+				</FormGroup>
+				<FormGroup label="Tags">
+					<TagInput
+						values={tags}
+						onChange={(values) => {
+							setTags(values);
+						}}></TagInput>
+				</FormGroup>
+				<Button type="submit">Search</Button>
+			</form>
 		</>
 	);
 };
